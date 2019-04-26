72fd7ece4ab417f31ebbc751128e883859da8ca7
Kentaro Fujiy <kentaro@manacadigital.com.br>
2019-04-26 22:55:59 +0000

    supergit

---
+++ config/deploy.rb
@@ -0,0 +1,96 @@
+#require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+require 'mina/rvm'    # for rvm support. (https://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   fetch(:shared_paths)    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :application_name, 'sotreq'
+set :domain, '142.93.63.58'
+set :deploy_to, '/var/www/sotreq'
+set :repository, 'https://github.com/kentarofujiy/sotreq-landingpage.git'
+set :user, 'appuser'
+set :branch, 'master'
+set :term_mode, nil
+
+# Optional settings:
+#   set :user, 'foobar'          # Username in the server to SSH to.
+#   set :port, '30000'           # SSH port number.
+#   set :forward_agent, true     # SSH forward_agent.
+
+set :shared_paths, [
+  'config/database.yml',
+  'config/application.yml',
+  'config/secrets.yml',
+  'tmp',
+  'log'
+]
+# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
+# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
+# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
+
+# This task is the environment that is loaded for all remote run commands, such as
+# `mina deploy` or `mina rake`.
+task :remote_environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use', 'ruby-2.5.0@sotreq'
+end
+
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  # command %{rbenv install 2.3.0 --skip-existing}
+  command %[mkdir -p "#{fetch(:shared_paths)}/shared/log"]
+  command %[chmod g+rx,u+rwx "#{fetch(:shared_paths)}/shared/log"]
+
+  command %[mkdir -p "#{fetch(:shared_paths)}/shared/config"]
+  command %[chmod g+rx,u+rwx "#{fetch(:shared_paths)}/shared/config"]
+
+  command %[mkdir -p "#{fetch(:shared_paths)}/shared/tmp"]
+  command %[chmod g+rx,u+rwx "#{fetch(:shared_paths)}/shared/tmp"]
+
+  command %[touch "#{fetch(:shared_paths)}/shared/config/database.yml"]
+  command %[touch "#{fetch(:shared_paths)}/shared/config/application.yml"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy do
+  # uncomment this line to make sure you pushed your local branch to the remote origin
+  # invoke :'git:ensure_pushed'
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    on :launch do
+      
+      in_path(fetch(:current_path)) do
+        command %{mkdir -p tmp/}
+        command %{touch tmp/restart.txt}
+      end
+    end
+  end
+
+  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+  # run(:local){ say 'done' }
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - https://github.com/mina-deploy/mina/tree/master/docs
